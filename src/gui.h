ea793aa6053918c756d962665729d0bfe1edf216
jake-greygoose <74213373+jake-greygoose@users.noreply.github.com>
2024-10-22 23:19:30 +0100

    gui code restructure

---
+++ src/gui.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <cstdint>
+#include <string>
+#include "Shared.h"
+#include "nexus/Nexus.h"
+#include "mumble/Mumble.h"
+#include "imgui/imgui.h"
+
+void DrawBar(float frac, int count, uint64_t totalDamage, const ImVec4& color, const std::string& eliteSpec, bool showDamage);
+void RenderSimpleRatioBar(int red, int green, int blue,
+    const ImVec4& colorRed, const ImVec4& colorGreen, const ImVec4& colorBlue,
+    const ImVec2& size);
+void RenderTeamData(int teamIndex, const TeamStats& teamData);
